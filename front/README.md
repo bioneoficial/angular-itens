f27012196d1e3f80141d116c9ba0d30fc536af3f
Joao Bione <joao.bione@hotmail.com>
2024-12-02 14:15:42 -0300

    Creating/fixing template, now i need  go back to my work

--- front/README.md
+++ front/README.md
@@ -1,4 +1,4 @@
-# NathanFede
+# Bione
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
 
--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'nathan-fede' title`, () => {
+  it(`should have the 'bione' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nathan-fede');
+    expect(app.title).toEqual('bione');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nathan-fede');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, bione');
   });
 });
